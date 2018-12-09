4088a5d2d7b8fd454f418ca86df86a56ca7700ff
Monesh123 <38658058+Monesh123@users.noreply.github.com>
2018-12-09 20:46:03 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,33 @@
+<?php 
+require("header.php");
+//require("checkUser.php")?>
+
+<script type="text/javascript">
+	document.getElementById("aaboutus").className="active";
+</script>
+
+ <div class="art-PostContent">
+ <div class="art-contentLayout">
+                   
+                            
+<p align="justify"><b>Start Learning</b> Are you looking to learn a new hobby? Are you preparing for competitive exams like CAT, GMAT, IIT, JEE etc? Or maybe you are a school/college student in need of extra tuition? Olcademy is a platform for all types of learners! All you need to start learning are an account and the hunger for knowledge! You can be assured of a safe and secure learning experience by teachers who have been put through a stringent background verification process and who are subject to compliance procedures throughout the duration of the program.
+ </p>
+
+<p align="justify">Do you consider yourself an expert in your hobby and wish to earn money by teaching it to others? Are you a teacher in an educational institution looking to earn a bit of extra money on the side through private tuitions? Maybe you are a LinkedIn influencer interested in influencing the world by sharing your expertise? Whatever you might wish to teach, Olcademy is the platform for you! Join us to have a secure platform to share your knowledge with the world!  </p>
+
+ <p>
+                                	<span class="art-button-wrapper">
+                                		<span class="l"> </span>
+                                		<span class="r"> </span>
+                                		<a class="art-button" href="contact.php">Contact Info...</a>
+                                	</span>
+                                </p>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+
+<?php require("footer.php")?>
